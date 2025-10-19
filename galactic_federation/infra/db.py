8eecd1d7c2b99d15c828fde68800d9730db73f97
Taylor B. <tayjaybabee@gmail.com>
2025-10-18 19:34:32 -0700

    Fix SQLAlchemy base import cycle

--- galactic_federation/infra/db.py
+++ galactic_federation/infra/db.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 from typing import Generator
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from sqlalchemy import create_engine
-from . import schemas
 from ..core.settings import get_settings
 
 class Base(DeclarativeBase):
